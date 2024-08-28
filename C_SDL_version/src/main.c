19d6132f851b22024c916738e9c4d1480156456c
twwhite84 <twwhite.pvt@gmail.com>
2024-08-28 17:50:20 +1200

    vector operations and backface culling

--- C_SDL_version/src/main.c
+++ C_SDL_version/src/main.c
@@ -10,7 +10,7 @@
 // triangle_t triangles_to_render[N_MESH_FACES]; // old
 triangle_t* triangles_to_render = NULL;
 
-vec3_t camera_position = {.x = 0, .y = 0, .z = -5};
+vec3_t camera_position = {0, 0, 0};
 
 float fov_factor = 640;
 
@@ -29,8 +29,8 @@ void setup(void) {
 
   // loads up our single mesh (that we have for now) with cube data
   // load_cube_mesh_data();
-
-  load_obj_file_data("./assets/f22.obj");
+  // load_obj_file_data("./assets/f22.obj");
+  load_obj_file_data("./assets/cube.obj");
 }
 
 /*----------------------------------------------------------------------------*/
@@ -60,54 +60,83 @@ vec2_t project(vec3_t point) {
 /*----------------------------------------------------------------------------*/
 
 void update(void) {
+  // projected 2d faces
+  triangles_to_render = NULL;
+
+  // cap the framerate
   int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - previous_frame_time);
   if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME) {
     SDL_Delay(time_to_wait);
   }
-
   previous_frame_time = SDL_GetTicks();
 
-  // initialise the array of triangles to render
-  triangles_to_render = NULL;
-
+  // our transformation will be a rotation
   mesh.rotation.x += 0.01;
   mesh.rotation.y += 0.01;
   mesh.rotation.z += 0.01;
 
-  // loop all triangle faces of the cube mesh
+  // for each face...
   int num_faces = array_length(mesh.faces);
   for (int i = 0; i < num_faces; i++) {
     face_t mesh_face = mesh.faces[i];
 
+    // ...get the vertices
     vec3_t face_vertices[3];
-    // mesh_face stores its vertices one-index, so -1 to fix off by one
     face_vertices[0] = mesh.vertices[mesh_face.a - 1];
     face_vertices[1] = mesh.vertices[mesh_face.b - 1];
     face_vertices[2] = mesh.vertices[mesh_face.c - 1];
 
-    // loop all three vertices of the face and apply the transform
-    triangle_t projected_triangle;
+    vec3_t transformed_vertices[3];
+
+    // apply the transform to each vertex
     for (int j = 0; j < 3; j++) {
       vec3_t transformed_vertex = face_vertices[j];
       transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
       transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
       transformed_vertex = vec3_rotate_z(transformed_vertex, mesh.rotation.z);
+      transformed_vertex.z += 5;  // default 0 is too zoomed
+      transformed_vertices[j] = transformed_vertex;
+    }
 
-      // translate the transformed vertex away from the camera (too zoomed in by
-      // default)
-      transformed_vertex.z -= camera_position.z;
+    // BACKFACE CULLING CHECK GOES HERE.
+    // 1. find vectors B-A and C-A (clockwise because we use LHCS)
+    // normalise vectors where you dont need magnitude. helps boost performance.
+    vec3_t vector_a = transformed_vertices[0]; /*   A   */
+    vec3_t vector_b = transformed_vertices[1]; /*  / \  */
+    vec3_t vector_c = transformed_vertices[2]; /* C---B */
+    vec3_t vector_ab = vec3_sub(vector_b, vector_a);
+    vec3_t vector_ac = vec3_sub(vector_c, vector_a);
+    vec3_normalise(&vector_ab);
+    vec3_normalise(&vector_ac);
 
-      // project the current vertex onto projection plane (i.e. our screen)
-      vec2_t projected_point = project(transformed_vertex);
+    // 2. take their cross product and find perpendicular normal N
+    // non-commutative, so order matters
+    vec3_t normal = vec3_cross(vector_ab, vector_ac);
+    vec3_normalise(&normal);
 
-      // scale and translate the projected point to the middle of the screen
+    // 3. find camera ray vector by subtracting camera position - point A
+    // this will point a vector from a towards camera
+    vec3_t camera_ray = vec3_sub(camera_position, vector_a);
+
+    // 4. take dot product between normal N and camera ray
+    // dot product is commutative, so order doesn't matter
+    float dot_normal_camera = vec3_dot(normal, camera_ray);
+
+    // 5. if dot product < 0, dont display that face
+    if (dot_normal_camera < 0) continue;
+
+    // project (i.e. create 2D coordinates for) the vertices
+    triangle_t projected_triangle;
+    for (int j = 0; j < 3; j++) {
+      vec2_t projected_point = project(transformed_vertices[j]);
+
+      // scale and translate the projected point relative to center of window
       projected_point.x += (window_width / 2);
       projected_point.y += (window_height / 2);
       projected_triangle.points[j] = projected_point;
     }
 
-    // save the projected triangle in the array of triangles to render
-    // triangles_to_render[i] = projected_triangle; // old
+    // save the 2d projected version of the face
     array_push(triangles_to_render, projected_triangle);
   }
 }
